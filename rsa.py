dbfb131b7b0f2b45ed7f12c8ed16b56b80f59ba4
Nishant Thakur <np01nt4a180024@islingtoncollege.edu.np>
2020-06-05 16:50:53 +0545

    Add files via upload

---
+++ rsa.py
@@ -0,0 +1,153 @@
+import time 
+import sys
+
+#Checking whether the user input is prime number or composite number. 
+def check_prime(number):
+    counter = 0
+    for divider in range(1,number+1):
+        remainder = number % divider
+        if remainder == 0:
+            counter += 1
+    return counter
+
+
+#Selecting First prime Number(p)
+def first_prime():
+    print('**********    FIRST PRIME NUMBER SELECTION *********\n')
+    while True:
+        try:
+            p = int(input("\nSelect first large random prime number: "))
+            no_of_divider = check_prime(p)
+            if no_of_divider == 2:
+                print(f"Congratulation, {p} is a prime number.\n")
+                return p
+                break
+            else:
+                print(f"Sorry, {p} is composite Number.\nTry Again...\n")
+        except:
+            print("Invalid Input. Try Again !!!")
+            
+
+#Selecting Second Prime Number(q)
+def second_prime():
+    print('*************    SECOND PRIME NUMBER SELECTION    ****************\n')
+    while True:
+        try:
+            q = int(input("Select second large random prime number: "))
+            no_of_divider = check_prime(q)
+            if no_of_divider == 2:
+                print(f'Congratulation Again, {q} is a prime Number.\n')
+                time.sleep(1)
+                return q
+                break
+            else:
+                print(f'Sorry, {q} is a composite Number.\nTry Again...\n')
+        except:
+            Print("Invalid Input. Try Again !!!")
+
+
+
+#Selecting public key(e) For Data Encryption
+def select_public_key():
+
+    print("*********    PUBLIC KEY SELECTION    ************")
+    print("Conditions : ")
+    print(f'     [1] : 1 < e < phi_n i.e. 1 < e < {phi_n}')
+    print(f'     [2] : gcd(e , phi_n) = 1.\n') #gcd = greatest common divisor
+    while True:
+        try:
+            e = int(input('Select your public key in the given range:'))
+            if e > 1 and e < phi_n:
+                no_of_divider = check_prime(e)
+
+                if no_of_divider == 2:
+                    print(f'public key : {e}\n')
+                    return e
+                    break
+                else:
+                    print(f'gcd({e} , {phi_n}) != 1.\nTry Again...')
+                    continue
+
+            else:
+                print(f'Please, Select Pubic Key in Range 1 < e < {phi_n}')
+                continue
+        except:
+            print('Please, Use Integer as Public key.')
+            continue
+
+
+
+#Calculating Private Key For Data Encryption.
+def private_key_selection():
+    for k in range(0,100):
+        d = (1 + k * phi_n) / e
+        if d.is_integer():
+            time.sleep(1)
+            print('Please Wait !!! Generating private key.')
+            time.sleep(1)
+            print('\n')
+            print(f'Private Key : {int(d)}\n')
+            return int(d)
+            break
+
+
+
+#Encrypting / Decrypting Data According to user Choice
+def encrypt_decrypt():
+    while True:
+        try:
+            time.sleep(1)
+            print('**********     WELCOME TO ENCRYPTION / DECRYPTION PHASE     ************\n')
+            time.sleep(0.6)
+            print('Press 1 for Encryption.\nPress 2 for Decryption.\nPress 3 to exit program\n')
+            time.sleep(0.6)
+            user_choice = input('Enter Your Choice : ')
+            print(f"User Choice : {user_choice}\n")
+
+            if user_choice == '1':
+                plain_text = int(input('Enter Plain text For Encryption: '))
+                time.sleep(1)
+                print('Please Wait !!! Generating Cipler Text...')
+                time.sleep(1)
+                cipher_text = (plain_text ** e) % n
+                print (f"Cipher Text : {cipher_text}\n")
+
+
+            if user_choice == '2':
+                cipher_text = int(input("Enter Cipher Text for Decryption : "))
+                plain_text = (cipher_text ** d) % n
+                print(f"Plain Text : {plain_text}\n")
+
+            if user_choice == '3':
+                print('Please Wait...')
+                time.sleep(1.4)
+                print('Program Ended Successfully.')
+                break
+
+        except:
+            print('Invalid Input..\n')
+            continue
+
+
+
+
+
+#Main Method for executing all the created functions.
+def final():
+    print('************     KEEP YOUR SYSTEM SECURE. ENCRYPT YOUR DATA WITH RSA     *************\n')
+    global p, q, n, phi_n, e, d 
+    p = first_prime()
+    q = second_prime()
+    n = p * q
+    print(f"The product of two prime numbers is [n : {n}]\n")
+    phi_n  = (p-1) * (q-1)
+    print(f"The value of phi_n is [phi_n : {phi_n}]\n")
+    e = select_public_key()
+    d = private_key_selection()
+    encrypt_decrypt()
+
+final()
+
+
+
+
